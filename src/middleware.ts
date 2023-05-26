ba3e3aa47677b27dbb0c0ed27d797434ed20f411
Alex Iglesias <alexiglesias@finsweet.com>
2023-05-26 17:48:32 +0200

    stream 6

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,6 @@
 import type { MiddlewareHandler } from 'hono';
 import { CoolerError } from './errors';
+import * as jose from 'jose';
 
 export const helloMiddleware: MiddlewareHandler = async (c, next) => {
   console.log('hey!');
@@ -13,14 +14,36 @@ export const superSecretWeapon: MiddlewareHandler = async (c, next) => {
 
 export const authenticateUser: MiddlewareHandler = async (c, next) => {
   // Check for Authentication header
-  const authentication = c.req.header('Authentication');
-  const user_id = authentication?.split('Bearer ')[1];
-  if (!user_id) {
+  const authorization = c.req.header('Authorization');
+  const access_token = authorization?.split('Bearer ')[1];
+  if (!access_token) {
     throw new CoolerError(401, 'Missing auhentication header');
   }
 
+  // Decode & validate the token
+  const url = new URL('https://cfw-stream.us.auth0.com/.well-known/jwks.json');
+
+  const JWKS = jose.createRemoteJWKSet(url);
+
+  const result = await jose.jwtVerify(access_token, JWKS, {
+    audience: [
+      'https://stream.finsweet.com',
+      'https://cfw-stream.us.auth0.com/userinfo',
+    ],
+    issuer: 'https://cfw-stream.us.auth0.com/',
+  });
+
+  console.log(result.key);
+  console.log(result.payload);
+  console.log(result.protectedHeader);
+
+  // Extract the user_id from the token
+  if (!result.payload.sub) {
+    throw new CoolerError(401, 'Unable to identify the user.');
+  }
+
   // Inject user info
-  c.set('user_id', user_id);
+  c.set('user_id', result.payload.sub);
 
   await next();
 };
